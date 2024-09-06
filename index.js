8f62b29e673f165e4e08ca48c641dd2b8e194f39
Chiselda <40883355+WajanaPiromnok@users.noreply.github.com>
2024-09-06 18:49:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/codapay/notification', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
